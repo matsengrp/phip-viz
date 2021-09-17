52c030932a4f3236ff3ef608f8e47ce79b5148ec
jgallowa07 <jaredgalloway07@gmail.com>
2021-09-17 11:29:30 -0700

    fiddling w/ cache

--- app-const.py
+++ app-const.py
@@ -5,6 +5,7 @@ from plotnine import *
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.colors import LogNorm
+import matplotlib.patches as patches
 import altair as alt
 import seaborn as sns
 import streamlit as st
@@ -12,47 +13,49 @@ import os
 import pickle
 import xarray as xr
 import pandas as pd
+import numpy as np
 import dask
 import phippery
 from phippery.tidy import tidy_ds
 
 
+if 'query_key_index' not in st.session_state:
+    st.session_state.query_key_index = 0 
 
-@st.cache(allow_output_mutation=True)
-def get_queries():
-    return pd.DataFrame({
-        "qkey": [],
-        "Type":[], 
-        "Condition": []
-    }).set_index("qkey")
-
-
+if 'drop_query_key_index' not in st.session_state:
+    st.session_state.drop_query_key_index = 0 
 
 # TODO Docstrings
+#@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+#def get_queries():
+#    return 
+#HERE#
+if 'queries' not in st.session_state:
+    st.session_state['queries'] = st.table(pd.DataFrame({
+            "qkey": [],
+            "Type":[], 
+            "Condition": []
+        }).set_index("qkey"))
+#st.write(st.session_state)
+
+# it could be all the new text input button keys
+# it could be sample peptide direct input 
+
 @st.cache(hash_funcs={xr.core.dataset.Dataset: dask.base.tokenize}, suppress_st_warning=True)
-def load_data(input_file_path: str, df, **kwargs):
+def load_data(input_file_path: str, sample_ids, peptide_ids, **kwargs):
+    st.write("Cache miss")
 
-    # no queries exist
-    #if len(get_queries()) == 0:
-    #    print("No Queries found: ")
-    #    print(get_queries())
-    #    ds = phippery.load(input_file_path)
-    #    return ds
-
-    #else:
-    print(f"in_load")
-    print(df)
     ds = phippery.load(input_file_path)
-    sid, pid = phippery.id_coordinate_from_query(ds, df)
-    # Call phippery.get_index_from_queries
-
-    st.write(f"using sample queries: {df}")
-    #return ds
-    # TODO actually query
+    # TODO Try, Catch Throw Informative Error
+    #try:
     return ds.loc[dict(
         sample_id=sid,
         peptide_id=pid
     )]
+    #except pd.core.computation.ops.UndefinedVariableError as e:
+    #    print(e)
+    #    return ds
+
 
 
 @st.cache(hash_funcs={xr.core.dataset.Dataset: dask.base.tokenize}, suppress_st_warning=True)
@@ -64,11 +67,11 @@ def compute_intervals(
 ):
 
     my_bar = st.progress(0)
+
     enrichments = ds[f"{enrichment}"]
-    
     # grab s and p tables
-    s_table = ds.sample_table.to_pandas().infer_objects()
-    p_table = ds.peptide_table.to_pandas().infer_objects()
+    s_table = ds.sample_table.to_pandas().replace(np.nan, "None")
+    p_table = ds.peptide_table.to_pandas().replace(np.nan, "None")
     p_table["Loc"] = p_table["Loc"].astype(int) 
     
     # Sum up the enrichments within windows
@@ -121,10 +124,25 @@ selected_input_file = st.sidebar.selectbox(
     input_file_list
 )
 
+ph = st.empty()
+
 def add_query_condition(*args, **kwargs):
 
-    st.session_state.query_key_index += 1
-    get_queries().loc[args[0]] = [args[1], st.session_state[args[0]]]
+    try:
+        #sid, pid = phippery.id_coordinate_from_query(ds, df)
+        #return ds.loc[dict(
+        #    sample_id=sid,
+        #    peptide_id=pid
+        #)]
+        ds.sample_table.to_pandas().query(st.session_state[args[0]])
+        st.session_state.query_key_index += 1
+        get_queries().loc[args[0]] = [args[1], st.session_state[args[0]]]
+    except Exception as e:
+        ph.text(f"{e}")
+
+        #st.sidebar.warning(f"{e}")
+        #except pd.core.computation.ops.UndefinedVariableError as e:
+        #    st.warning(f"{e}")
 
 
 def drop_query_condition(*args, **kwargs):
@@ -138,43 +156,54 @@ def drop_query_condition(*args, **kwargs):
         get_queries().drop(to_drop, axis=0, inplace=True)
 
 
-with st.sidebar.expander('Sample & Peptide Selection Condtions'):
-    st.dataframe(get_queries())
-    #print(get_queries())
-    #st.table(get_queries())
+#with st.sidebar.expander('Sample & Peptide Selection Condtions'):
+#print(get_queries())
+#st.table(get_queries())
+#if "clicked_query_type_flag" not in st.session_state:
+#    st.session_state.clicked_query_type_flag = False
+#def query_type_flag_fn():
+#    st.session_state.clicked_query_type_flag = True
 
-    qtype = st.selectbox('query type', ["sample", "peptide"])
+qtype = st.sidebar.selectbox(
+        'query type', 
+        ["sample", "peptide"],
+        #on_change = query_type_flag_fn
+)
+#st.write(st.session_state.clicked_query_type_flag)
 
-    if 'query_key_index' not in st.session_state:
-        st.session_state.query_key_index = 0 
-    num_queries = st.session_state.query_key_index
 
-    if 'drop_query_key_index' not in st.session_state:
-        st.session_state.drop_query_key_index = 0 
-    num_dropped_queries = st.session_state.drop_query_key_index
+#if 'query_key_index' not in st.session_state:
+#    st.session_state.query_key_index = 0
+num_queries = st.session_state.query_key_index
 
-    # Add Query
-    st.text_input(
-        label=f"Condition: {num_queries+1}", 
-        key=f"sq_{num_queries+1}", 
-        on_change=add_query_condition,
-        args=tuple([f"sq_{num_queries+1}", qtype])
-    )
+#if 'drop_query_key_index' not in st.session_state:
+#    st.session_state.drop_query_key_index = 0
+num_dropped_queries = st.session_state.drop_query_key_index
 
-    # Remove Query
-    st.text_input(
-        label=f"Remove Condition (by key)", 
-        key=f"rm_key_{num_dropped_queries}", 
-        on_change=drop_query_condition,
-        args=tuple([f"rm_key_{num_dropped_queries}"])
-    )
+# Add Query
+warn_query_placeholder = st.empty()
+st.sidebar.text_input(
+    label=f"Condition: {num_queries+1}",
+    key=f"sq_{num_queries+1}",
+    on_change=add_query_condition,
+    args=tuple([f"sq_{num_queries+1}", qtype])
+)
+
+st.sidebar.dataframe(get_queries())
+# Remove Query
+st.sidebar.text_input(
+    label=f"Remove Condition (by key)",
+    key=f"rm_key_{num_dropped_queries}",
+    on_change=drop_query_condition,
+    args=tuple([f"rm_key_{num_dropped_queries}"])
+)
+
+
+#sid, pid = phippery.id_coordinate_from_query(ds, df)
 
 # Load data (cached if no change)
-ds = load_data(selected_input_file, get_queries())
 
-##################################################
-# Data
-##################################################
+ds = load_data(selected_input_file, get_queries())
 
 unique_types = ["Heatmap"]
 selected_types = st.sidebar.multiselect(
@@ -183,103 +212,107 @@ selected_types = st.sidebar.multiselect(
     default=unique_types
 )
 
-if "Heatmap" in selected_types:
 
-    with st.form("dt"):
-        st.write(f"Data Transform")
+#if "Heatmap" in selected_types and not st.session_state.clicked_query_type_flag:
+if "Heatmap" in selected_types: 
 
-        @st.cache(hash_funcs={xr.core.dataset.Dataset: dask.base.tokenize}, suppress_st_warning=True)
-        def get_valid_enr(ds):
+    #with left_column:
+    with st.expander('Heatmap Settings', expanded=True):
+        with st.form("dt"):
+            st.write(f"Transform Data")
+
+            #@st.cache(hash_funcs={xr.core.dataset.Dataset: dask.base.tokenize}, suppress_st_warning=True)
+            #def get_valid_enr(ds):
             enrichment_options = []
             for dt in set(list(ds.data_vars)) - set(["sample_table", "peptide_table"]):
                 if ds[dt].values.flatten().min() != ds[dt].values.flatten().max():
                     enrichment_options.append(dt)
-            return enrichment_options
+            #    return enrichment_options
+            
+            enrichment_options = get_valid_enr(ds)
+            enrichment = st.selectbox(
+                "Normalization layer",
+                enrichment_options
+            )
+            
+            window_size_options = list(range(1, len(ds.peptide_id.values)))
+            window_size = st.selectbox(
+                "Peptide Window Size",
+                window_size_options
+            )
+            
+            agg_choices = ['sum', 'median', 'mean']
+            agg_func = st.selectbox(
+                "Window Aggregation Function",
+                agg_choices
+            )
+            
+            agg_samples, enrichment_columns = compute_intervals(
+                ds, 
+                enrichment,
+                window_size,
+                agg_func
+            )
+
+            # How to group the samples
+            sample_groupby_choices = list(ds.sample_metadata.values)
+            sample_groupby = st.selectbox(
+                "Sample Order By",
+                ["Unordered"] + sample_groupby_choices,
+                index=0
+            )
 
 
-        
-        enrichment_options = get_valid_enr(ds)
-        enrichment = st.selectbox(
-            "Normalization layer",
-            enrichment_options
-        )
-        
-        window_size_options = list(range(1, len(ds.peptide_id.values)))
-        window_size = st.selectbox(
-            "Peptide Window Size",
-            window_size_options
-        )
-        
-        agg_choices = ['sum', 'median', 'mean']
-        agg_func = st.selectbox(
-            "Window Aggregation Function",
-            agg_choices
-        )
-        
-        agg_samples, enrichment_columns = compute_intervals(
-            ds, 
-            enrichment,
-            window_size,
-            agg_func
-        )
-        submitted = st.form_submit_button("Submit")
+            if sample_groupby != "Unordered":
+                sample_order = []
+                for g, g_df in agg_samples.groupby(
+                        [sample_groupby]
+                        ):
+                    sample_order.extend(g_df.index)    
+                assert len(sample_order) == len(agg_samples)
+                agg_samples = agg_samples.reindex(sample_order)
 
-    with st.form("p"):
-        st.write(f"Plot Aesthetics")
+            ## Range of values to viz
+            s_e = copy.deepcopy(agg_samples[enrichment_columns])
 
-        # How to group the samples
-        sample_groupby_choices = ds.sample_metadata.values
-        sample_groupby = st.selectbox(
-            "Sample Order By",
-            sample_groupby_choices,
-            index=0
-        )
+            a = int(s_e.values.flatten().min())
+            b = int(s_e.values.flatten().max())
+            values = st.slider(
+                "Min Max Enrichment",
+                a,
+                b,
+                (a,b)
+            )
+            s_e[s_e < values[0]] = values[0]
+            s_e[s_e > values[1]] = values[1]
 
-        sample_order = []
-        for g, g_df in agg_samples.groupby(
-                [sample_groupby]
-                ):
-            sample_order.extend(g_df.index)    
-        agg_samples = agg_samples.reindex(sample_order)
+            # Colormap
+            heatcolormap_choices = [
+                    "RdPu",
+                    "bwr",
+                    "bone",
+                    "pink"
+            
+            ]
+            heatcolormap = st.selectbox(
+                "Colormap",
+                heatcolormap_choices,
+                0
+            )
+            hcmap = getattr(cm, heatcolormap)
 
-        ## Range of values to viz
-        s_e = copy.deepcopy(agg_samples[enrichment_columns])
+            norm = st.selectbox(
+                "Color Scale",
+                ["Log", "Linear"],
+                0
+            )
 
-        a = int(s_e.values.flatten().min())
-        b = int(s_e.values.flatten().max())
-        values = st.slider(
-            "Min Max Enrichment",
-            a,
-            b,
-            (a,b)
-        )
-        s_e[s_e < values[0]] = values[0]
-        s_e[s_e > values[1]] = values[1]
+            submitted = st.form_submit_button("Submit")
 
-        # Colormap
-        heatcolormap_choices = [
-                "RdPu",
-                "bwr",
-                "bone",
-                "pink"
-        
-        ]
-        heatcolormap = st.selectbox(
-            "Colormap",
-            heatcolormap_choices,
-            0
-        )
-        hcmap = getattr(cm, heatcolormap)
+        #with right_column:
+    #with st.spinner(text='In progress'):
 
-        norm = st.selectbox(
-            "Color Scale",
-            ["Log", "Linear"],
-            0
-        )
-
-        submitted = st.form_submit_button("Submit")
-
-    fig, ax = plt.subplots(figsize=[10, 14])
+    fig, ax = plt.subplots(figsize=[8, 8])
     cbar_kws = dict(use_gridspec=False, location="right", label=enrichment)
     sns.heatmap(
             s_e, 
@@ -288,33 +321,30 @@ if "Heatmap" in selected_types:
             cbar_kws=cbar_kws, 
             norm=LogNorm() if norm=="Log" else None
     )
+
+    if sample_groupby != "Unordered":
+
+        base=0
+        for g, g_df in agg_samples.groupby([sample_groupby]):
+            
+            height = len(g_df.index)
+        
+            #rect_v = patches.Rectangle(
+            #        (-100, base),
+            #        width=70,
+            #        height=height,
+            #        clip_on=False, 
+            #        linewidth=1, 
+            #        edgecolor='black',
+            #        facecolor="None"
+            #)
+            ax.get_yaxis().set_visible(False)
+            ax.axhline(base + height, lw=2, color="black")
+            #ax.text(-1.0*float(window_size), (base+(base+height))/2, g, rotation=90, va="center", ha="right", size=14)
+            ax.text(-1.0, (base+(base+height))/2, g, va="center", ha="right", size=14)
+            #ax.text(-1.0, (base+(base+height))/2, g, rotation=90, va="center", ha="right", size=14)
+            #ax.add_patch(rect_v)
+            #axd["A"].set_xlabel("Amino acid position")
+            base = base + height
+
     st.write(fig)
-
-    #else:
-    #    sns.heatmap(s_e, ax = ax, vmin=values[0], vmax=values[1], cmap=hcmap, cbar_kws=cbar_kws)
-
-    #base=0
-    #for g, g_df in s_table.groupby():
-    #    
-    #    height = len(g_df.index)
-    #
-    #    rect_v = patches.Rectangle(
-    #            (-10, base),
-    #            width=7,
-    #            height=height,
-    #            clip_on=False, 
-    #            linewidth=1, 
-    #            edgecolor='black',
-    #            facecolor="None"
-    #    )
-    #    axd["A"].axhline(base + height, lw=2, color="black")
-    #    axd["A"].text(-6.0, (base+(base+height))/2, label, rotation=90, va="center", ha="center", size=14)
-    #    axd["A"].add_patch(rect_v)
-    #    #axd["A"].set_xlabel("Amino acid position")
-    #    base = base + height
-
-
-
-
-
-
